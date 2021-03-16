bc0efd7f1d1cdea4ce0b5168be3aeca4138e95ae
sys-walker <pere.rollon@gmail.com>
2021-03-16 14:45:21 +0100

    Url /todo/

--- td/td/urls.py
+++ td/td/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-
+from django.urls import path,include
+from webtodo import views
 urlpatterns = [
+	path('', views.home),
+	path('todo/', include('webtodo.urls')),
     path('admin/', admin.site.urls),
 ]
--- td/webtodo/views.py
+++ td/webtodo/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+#CONTROLLER
+
+def home(r):
+	return HttpResponse("OK")
